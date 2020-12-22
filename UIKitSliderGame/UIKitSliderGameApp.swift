6879809b604beb2c941f45448e0ec778ff4fec29
Evgeny Novgorodov <greatly01@ya.ru>
2020-12-22 19:41:45 +0500

    Initial Commit

---
+++ UIKitSliderGame/UIKitSliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSliderGameApp.swift
+//  UIKitSliderGame
+//
+//  Created by User on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
